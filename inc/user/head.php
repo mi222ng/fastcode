81be7e02a316deab90b25d74fd5b7bcb82ae337e
mi222ng <mi222ng@naver.com>
2023-04-17 17:55:19 +0900

    수정

--- inc/user/head.php
+++ inc/user/head.php
@@ -8,7 +8,7 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>fastcode</title>
+        <title>FASTCODE</title>
         <link rel="apple-touch-icon" sizes="57x57" href="http://mzeeyy.dothome.co.kr/fastcode/user/img/favicon/apple-icon-57x57.png">
         <link rel="apple-touch-icon" sizes="60x60" href="http://mzeeyy.dothome.co.kr/fastcode/user/img/favicon/apple-icon-60x60.png">
         <link rel="apple-touch-icon" sizes="72x72" href="http://mzeeyy.dothome.co.kr/fastcode/user/img/favicon/apple-icon-72x72.png">
